bb2cf8e80f4a252e0755191ea11206ca7ee73056
Japb <Japb@github.com>
2018-02-25 12:33:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/toshiba/myapplication2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.toshiba.myapplication2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStar(){
+        super.onStar();
+        Toast.makeText(this, "onStar", Toast.LENGTH_SHORT).show();
+        //La actividad se ha vuelto visible (ahora se "reanuda").
+
+
+
+
+    }
+}
